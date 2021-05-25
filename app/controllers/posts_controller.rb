0ccd6a86cb13fbd79bf27d2bdd8d1a9989020bf2
djsteve33 <swomble@aol.com>
2021-05-25 16:19:36 -0400

    commit tree 08b30618e9f554c4a9eaccacd5c68f4209065d49

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
+  #helper_method :params
 
   def index
+    # provide a list of authors to the view for the filter control
+    @authors = Author.all
+
+  # filter the @posts list based on user input
+  if !params[:author].blank?
+    @posts = Post.by_author(params[:author])
+  elsif !params[:date].blank?
+    if params[:date] == "Today"
+      @posts = Post.from_today
+    else
+      @posts = Post.old_news
+    end
+  else
+    # if no filters are applied, show all posts
     @posts = Post.all
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,17 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
+  def self.by_author(author_id)
+    where(author: author_id)
+  end 
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+  
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end 
 
   private
 
